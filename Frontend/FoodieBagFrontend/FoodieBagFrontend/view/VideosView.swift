73be9a32128d8eb7272d9c78119772bd0abb39ed
heidi <heidialbara>
2023-11-18 11:06:09 +0100

    add Tiktokscroll behaviour

--- Frontend/FoodieBagFrontend/FoodieBagFrontend/view/VideosView.swift
+++ Frontend/FoodieBagFrontend/FoodieBagFrontend/view/VideosView.swift
@@ -5,11 +5,29 @@
 //  Created by Heidi Albarazi on 18.11.23.
 //
 
+
+// Tiktok scroll behaviour
 import SwiftUI
 
 struct VideosView: View {
     var body: some View {
-        Text("Video, World!")
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 0) {
+                ForEach(0..<100) { i in
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.blue.opacity(0.6))
+                            .containerRelativeFrame([.horizontal, .vertical])
+                        Text("Video \(i+1)")
+                            .font(.title)
+                            .bold()
+                    }
+                }
+            }
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.paging)
+        .ignoresSafeArea()
     }
 }
 
